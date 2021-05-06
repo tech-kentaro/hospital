ffece86e2c1a6d5796b6dd98b1a17da0e44b6bfb
tech-kentaro <gekkouaria@icloud.com>
2021-05-06 09:54:01 +0900

    hero js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,22 @@
-$('.global__btn').click(function() {
+$('.hero__slider').slick({
+  fade: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  speed: 1000,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  pauseOnFocus: false,
+  pauseOnHover: false,
+  pauseOnDotsHover: false,
+});
+
+$('.hero__slider').on('touchmove', function(event, slick, currentSlide, nextSlide) {
+  $('.hero__slider').slick('slickPlay');
+});
+
+$('.global__btn, .global__li a').click(function() {
   if ($('body').hasClass('active')) {
     $('body').removeClass('active');
     $('.global__time').fadeIn();
@@ -9,20 +27,17 @@ $('.global__btn').click(function() {
 });
 
 $('.global__time').click(function() {
+  const clock = $('.fa-clock');
+  const times = $('.fa-times');
   if ($('body').hasClass('activeTime')) {
     $('body').removeClass('activeTime');
     $('.global__btn').fadeIn();
+    times.removeClass('dBlock');
+    clock.addClass('dBlock');
   } else {
     $('body').addClass('activeTime');
     $('.global__btn').fadeOut();
-    const clock = $('.fa-clock');
-    const times = $('.fa-times');
-    if (clock.hasClass('dBlock')) {
-      clock.removeClass('dBlock');
-      times.addClass('dBlock');
-    } else {
-      times.removeClass('dBlock');
-      clock.addClass('dBlock')
-    }
+    clock.removeClass('dBlock');
+    times.addClass('dBlock');
   }
 });
