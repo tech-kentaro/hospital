ebd6f43d5980e2a00b06bde65ecc14bd0395084c
tech-kentaro <gekkouaria@icloud.com>
2021-05-06 16:55:33 +0900

    modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,45 @@
+// const access = $.cookie('access');
+// if (!access) {
+//   flag = true;
+//   $.cookie('access', false);
+// } else {
+//   flag = false;
+// }
+
+// $('.modal').modaal({
+//   start_open: flag,
+//   overlay_close: true,
+//   before_open: function() {
+//     $('html').css('overflow-y', 'hidden');
+//   },
+//   after_open: function() {
+//     $('html').css('overflow-y', 'scroll');
+//   }
+// });
+
+var access = $.cookie("access");
+if (!access) {
+  flag = true;
+  $.cookie("access", false);
+} else {
+  flag = false;
+}
+
+//モーダル表示
+$(".modal").modaal({
+  start_open: flag, // ページロード時に表示するか
+  overlay_close: true, //モーダル背景クリック時に閉じるか
+  background: "#000", // 背景色
+  before_open: function () {
+    // モーダルが開く前に行う動作
+    $("html").css("overflow-y", "hidden"); /*縦スクロールバーを出さない*/
+  },
+  after_close: function () {
+    // モーダルが閉じた後に行う動作
+    $("html").css("overflow-y", "scroll"); /*縦スクロールバーを出す*/
+  },
+});
+
 function mediaQuery() {
   const winW = $(window).width();
   if (winW < 1024) {
